2c3062613917ece623c042e23a26ac1ff2545ecd
Ubuntu <ubuntu@ip-172-31-69-107.ec2.internal>
2021-04-10 03:14:58 +0000

    Complete userWatchMovie function

--- src/UserLikesService/UserLikesHandler.h
+++ src/UserLikesService/UserLikesHandler.h
@@ -369,13 +369,67 @@ int64_t UserLikesServiceHandler::GetMovieRating(const std::string& movie_id) {
 }
 
 void UserLikesServiceHandler::UserWatchMovie(const int64_t user_id, const std::string& movie_id) {
-	// TO DO: put 0 in rating position for user and movie
-	if (user_id != 0 && movie_id != "") {
-		// put 0 in rating for movie
+	// Get mongo client
+	mongoc_client_t *mongodb_client = mongoc_client_pool_pop(_mongodb_client_pool);
+	if (!mongodb_client) {
+	  ServiceException se;
+	  se.errorCode = ErrorCode::SE_MONGODB_ERROR;
+	  se.message = "Failed to pop a client from MongoDB pool";
+	  throw se;
 	}
-	else {
-		return;
+
+	// Get mongo collection
+	auto collection = mongoc_client_get_collection(mongodb_client, "likes", "user-likes");
+	if (!collection) {
+	  ServiceException se;
+	  se.errorCode = ErrorCode::SE_MONGODB_ERROR;
+	  se.message = "Failed to create collection user from DB likes";
+	  mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
+	  throw se;
 	}
+	
+	// Find out if user has already watched or rated movie
+	bson_t *query = bson_new();
+	BSON_APPEND_INT64(query, "user_id", user_id);
+	BSON_APPEND_UTF8(query, "movie_id", movie_id.c_str());
+	
+	mongoc_cursor_t *cursor = mongoc_collection_find_with_opts(collection, query, nullptr, nullptr);
+	const bson_t *doc;
+	bool found = mongoc_cursor_next(cursor, &doc);
+	
+	// Only if user has not already rated/watched movie do we add a record
+	if (!found) {
+		bson_t *add_movie = bson_new();
+		BSON_APPEND_INT64(add_movie, "user_id", user_id);
+		BSON_APPEND_UTF8(add_movie, "movie_id", movie_id);
+		BSON_APPEND_INT64(add_movie, "rating", 0);
+		
+		bson_error_t error;
+		bool addMovie = mongoc_collection_insert_one(collection, add_movie, nullptr, nullptr, &error);
+		
+		if (!addMovie) {
+			LOG(error) << "Failed to add user-likes record for user " << std::to_string(user_id) <<
+			" and movie " << movie_id << " to MongoDB: " << error.message;
+			ServiceException se;
+			se.errorCode = ErrorCode::SE_MONGODB_ERROR;
+			se.message = error.message;
+			// Cleanup mongo
+			bson_destroy(add_movie);
+			bson_destroy(query);
+			mongoc_cursor_destroy(cursor);
+			mongoc_collection_destroy(collection);
+			mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
+			throw se;
+		}
+		bson_destroy(add_movie);
+	}
+	
+	// Cleanup mongo
+	bson_destroy(query);
+	mongoc_cursor_destroy(cursor);
+	mongoc_collection_destroy(collection);
+	mongoc_client_pool_push(_mongodb_client_pool, mongodb_client);
+	mongoc_cleanup();
 }
 
 void UserLikesServiceHandler::AddUser(const std::string& user_name) {
