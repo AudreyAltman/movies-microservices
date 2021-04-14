4a46dd6f5452b36a9c8e68ef254913bf8d8648cb
AudreyAltman <audreykaltman@gmail.com>
2021-04-13 21:28:31 -0400

    update readme, script

--- script/upload_recommendation.sh
+++ script/upload_recommendation.sh
@@ -1,4 +1,4 @@
 for i in {1..21}
 do
-  curl -d "user_id="$i"&movie_id=aaa&movie_id=def" localhost:8080/recommendations/upload
+  curl -d "user_id="$i"&movie_id=aaa&movie_id=ddd" localhost:8080/recommendations/upload
 done
